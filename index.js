3aeb30278086a7b68e009ba553f288cbe3dd8bcc
Ashley Jackson <ashleyj.jacks@gmail.com>
2022-04-18 19:10:46 -1000

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const inq = require('inquirer')
+const uuidv4 = require('uuid').v4
+const colors = require('colors')
+const Table = require('cli-table3')
+const _ = require('lodash')
+
+console.log(uuidv4())
